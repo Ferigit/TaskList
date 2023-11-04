526187487137ad8618b54479459b4deb00c94c34
Ferigit <faramarz.razmi@gmail.com>
2023-11-04 20:05:00 +0300

    feat: add tasks page and new task logic and complete a task and remove a task login and ui

--- components/business/Tasks/AddTaskForm.tsx
+++ components/business/Tasks/AddTaskForm.tsx
@@ -1,27 +1,76 @@
-import React, { useState } from "react";
+import React from "react";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { FormProvider, useForm } from "react-hook-form";
+import clsx from "classnames";
+import { TextInput, Button } from "components/common";
+import { addTask } from "services/tasks";
+import { v4 } from "uuid";
+import { TaskFormType } from "interfaces/task";
 
-const AddTaskForm = () => {
-  const [taskTitle, setTaskTitle] = useState("");
-  const [taskDescription, setTaskDescription] = useState("");
+const validationSchema = Yup.object({
+  title: Yup.string().required(),
+  description: Yup.string().required(),
+}).required();
 
-  const handleAddTask = () => {};
+const AddTaskForm = ({ tasks, setTasks, onSubmit }) => {
+  const formMethods = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+  const {
+    register,
+    reset,
+    formState: { errors },
+  } = formMethods;
+
+  const handleAddTask = async (data: TaskFormType) => {
+    const { title, description } = data;
+
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      completed: false,
+    };
+    try {
+      const res = await addTask(newTask);
+      if (res.id) {
+        setTasks([...tasks, res]);
+        reset();
+        onSubmit();
+      }
+    } catch (err: any) {}
+  };
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Task Title"
-        value={taskTitle}
-        onChange={(e) => setTaskTitle(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Task Description"
-        value={taskDescription}
-        onChange={(e) => setTaskDescription(e.target.value)}
-      />
-      <button onClick={handleAddTask}>Add Task</button>
-    </div>
+    <FormProvider {...formMethods}>
+      <form
+        onSubmit={formMethods.handleSubmit(handleAddTask)}
+        className={clsx("bg-secondary rounded-md flex flex-col gap-3 w-full")}
+      >
+        <h2 className="text-center font-medium text-xl">ADD NEW TASK</h2>
+        <TextInput
+          label="Title"
+          register={register}
+          name="title"
+          placeholder="Task title"
+          errors={errors}
+        />
+        <TextInput
+          label="Description"
+          register={register}
+          placeholder="Task description"
+          name="description"
+          errors={errors}
+        />
+        <Button
+          type="submit"
+          className="text-white rounded-full w-full bg-primary h-12"
+        >
+          <p>ADD</p>
+        </Button>
+      </form>
+    </FormProvider>
   );
 };
 
